5951b5b967962992d91578f53b12e4796e954aee
marcvonderlinde <marc.vonderlinde@ugent.be>
2022-11-24 12:11:56 +0100

    Blogpost Template and Color black added

---
+++ components/specificComponents/Blogpost/Blogpost.js
@@ -0,0 +1,27 @@
+﻿import React, { Component } from "react";
+import css from "./Blogpost.module.scss";
+import { storyblokEditable } from "@storyblok/react";
+import {RichTextToHTML} from "../../../functions/storyBlokRichTextRenderer";
+
+export default class Blogpost extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<>
+				<div {...storyblokEditable(this.props.blok)} className={css["Blogpostitem"]}>
+					<div className={css["Blogpostheader"]}>
+						<span className={css["Blogpostdate"]}>{this.props.blok.date}</span>
+						<span className={css["Blogposttitle"]}>{this.props.blok.title}</span>
+					</div>
+					<div className={css["Blogpostitemcontent"]}>
+						{RichTextToHTML({ document: this.props.blok.description })}
+					</div>
+				</div>
+			</>
+		);
+	}
+}
