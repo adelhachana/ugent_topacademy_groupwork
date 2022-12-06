8df0703a13fb9ddd9d794de89a272afbe69114ba
marcvonderlinde <marc.vonderlinde@ugent.be>
2022-12-06 15:08:17 +0100

    tawk integration 02

--- components/technicalComponents/HeadComponent/HeadComponent.js
+++ components/technicalComponents/HeadComponent/HeadComponent.js
@@ -67,7 +67,7 @@ export default class HeadComponent extends Component {
 					(function(){
 					var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 					s1.async=true;
-					s1.src='https://embed.tawk.to/636e4797daff0e1306d6eb31';
+					s1.src='https://embed.tawk.to/db906fb8eccf5e6560a02e434623acb7e059ecf0';
 					s1.charset='UTF-8';
 					s1.setAttribute('crossOrigin','*');
 					s0.parentNode.insertBefore(s1,s0);})();`,
