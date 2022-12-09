eb407228cf7859745e1084b08ac5062022c1d659
marcvonderlinde <marc.vonderlinde@ugent.be>
2022-12-09 19:41:39 +0100

    font change to Wire One

--- components/technicalComponents/HeadComponent/HeadComponent.js
+++ components/technicalComponents/HeadComponent/HeadComponent.js
@@ -60,7 +60,7 @@ export default class HeadComponent extends Component {
 				<link rel="dns-prefetch" href="https://cdn.materialdesignicons.com" />
 				{this.props.socialTags && Object.entries(this.props.socialTags).map(getTagElement)}
 				{this.state.loaded && <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@3.9.97/css/materialdesignicons.min.css" />}
-				{this.state.loaded && <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Silkscreen:wght@400;700&display=swap" rel="stylesheet" />}
+				{this.state.loaded && <link href="'https://fonts.googleapis.com/css2?family=Special+Elite&family=Wire+One&display=swap'" rel="stylesheet" />}
 			</Head>
 			<Script id="tawktoscriptwrapper" dangerouslySetInnerHTML={{
 				__html: `var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
