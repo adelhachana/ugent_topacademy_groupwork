f131d3006a1232904c209cafee227afa6da546f7
marcvonderlinde <marc.vonderlinde@ugent.be>
2023-01-13 10:43:13 +0100

    blogpost trial & error 1

--- components/specificComponents/Blogpost/Blogpost.js
+++ components/specificComponents/Blogpost/Blogpost.js
@@ -1,9 +1,8 @@
 ﻿import React, { Component } from "react";
-import css from "./Blogpost.module.scss";
-import { storyblokEditable } from "@storyblok/react";
-import {RichTextToHTML} from "../../../functions/storyBlokRichTextRenderer";
+import css from "./Person.module.scss";
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
-export default class Blogpost extends Component {
+export default class Person extends Component {
 
 	constructor(props) {
 		super(props);
@@ -12,16 +11,32 @@ export default class Blogpost extends Component {
 	render() {
 		return (
 			<>
-				<div {...storyblokEditable(this.props.blok)} className={css["Blogpostitem"]}>
-					<div className={css["Blogpostheader"]}>
-						<span className={css["Blogpostdate"]}>{this.props.blok.date}</span>
-						<span className={css["Blogposttitle"]}>{this.props.blok.title}</span>
-					</div>
-					<div className={css["Blogpostitemcontent"]}>
-						{RichTextToHTML({ document: this.props.blok.description })}
+				<div {...storyblokEditable(this.props.blok)} className={css["wrapper"]}>
+					<div className={css["content"]}>
+						<div className={[css["box"], css["head"]].join(" ")}>
+							<h1>{this.props.blok.title} {this.props.blok.lastname} {this.props.blok.firstname}</h1>
+						</div>
+						<div className={[css["box"], css["sidebar"]].join(" ")}>
+							<div className={css["personalimage"]}><img src={this.props.blok.image.filename} /></div>
+							<div className={css["personaldetails"]}>
+								<div className={css["personaldetailitem"]}>{this.props.blok.title} {this.props.blok.firstname} {this.props.blok.lastname}</div>
+								<div className={css["personaldetailitem"]}>{this.props.blok.dateofbirth}</div>
+								<div className={css["personaldetailitem"]}>{this.props.blok.location}</div>
+							</div>
+						</div>
+						<div className={[css["box"], css["experience"]].join(" ")}>
+							<h2>Experience</h2>
+							{this.props.blok.experiences.map((nestedBlok) => (
+								<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+							))}
+						</div>
+						<div className={[css["box"], css["foot"]].join(" ")}>
+							<div>&copy; {this.props.blok.firstname} {this.props.blok.lastname} {new Date().getFullYear()}</div>
+						</div>
 					</div>
 				</div>
 			</>
 		);
+		
 	}
 }
