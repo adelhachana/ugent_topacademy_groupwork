0bdc43c458b8e263dba6fd6ce16820c5f9be9fc1
marcvonderlinde <marc.vonderlinde@ugent.be>
2023-01-13 13:19:48 +0100

    blogpost - teacher - template 5_1

--- components/specificComponents/Blogpost/Blogpost.js
+++ components/specificComponents/Blogpost/Blogpost.js
@@ -1,42 +1,45 @@
 ﻿import React, { Component } from "react";
 import css from "./Blogpost.module.scss";
+import Headermenu from "../../genericComponents/Headermenu/Headermenu";
+import Hero from "../../genericComponents/Hero/Hero";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
 export default class Blogpost extends Component {
 
 	constructor(props) {
 		super(props);
+		this.props.blok.title = this.props.blok.firstname + " " + this.props.blok.lastname
+		this.props.blok.tagline = "Location: " + this.props.blok.location;
 	}
 
 	render() {
 		return (
-			<>
-				<div {...storyblokEditable(this.props.blok)} className={css["wrapper"]}>
-					<div className={css["content"]}>
-						<div className={[css["box"], css["head"]].join(" ")}>
-							<h1>{this.props.blok.title} {this.props.blok.lastname} {this.props.blok.firstname}</h1>
+			<div {...storyblokEditable(this.props.blok)}>
+				<Headermenu blok={this.props.menu.content}></Headermenu>
+
+				<main>
+					<Hero blok={this.props.blok} contentTypeTag="course" />
+					<div className={css["blogpost-page__main-content"]}>
+						<div id="blogpost-page__short-description" key="blogpost-page__short-description" className={css["blogpost-page__short-description"]}>
+							<section className={css["rich-text-section--with-navigator"]}>
+								<h2 className={css["rich-text-section__title"]}>My Life</h2>
+								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.bio })}</div>
+							</section>
 						</div>
-						<div className={[css["box"], css["sidebar"]].join(" ")}>
-							<div className={css["blogpostimage"]}><img src={this.props.blok.image.filename} /></div>
-							<div className={css["blogpostdetails"]}>
-								<div className={css["blogpostdetailitem"]}>{this.props.blok.title} {this.props.blok.firstname} {this.props.blok.lastname}</div>
-								<div className={css["blogpostdetailitem"]}>{this.props.blok.dateofbirth}</div>
-								<div className={css["blogpostdetailitem"]}>{this.props.blok.location}</div>
-							</div>
-						</div>
-						<div className={[css["box"], css["experience"]].join(" ")}>
-							<h2>Experience</h2>
-							{this.props.blok.experiences.map((nestedBlok) => (
-								<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-							))}
-						</div>
-						<div className={[css["box"], css["foot"]].join(" ")}>
-							<div>&copy; {this.props.blok.firstname} {this.props.blok.lastname} {new Date().getFullYear()}</div>
+						<div id="blogpost-page__short-description" key="blogpost-page__short-description" className={css["blogpost-page__short-description"]}>
+							<section className={css["rich-text-section--with-navigator"]}>
+							<h2 className={css["rich-text-section__title"]}>Experience</h2>
+								{this.props.blok.experiences && this.props.blok.experiences.map((nestedBlok) => (
+									<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+								))}
+							</section>
 						</div>
 					</div>
-				</div>
-			</>
+
+				</main>
+			</div>
 		);
-		
+
 	}
 }
